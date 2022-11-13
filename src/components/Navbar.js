af4362842f81f35bda55898306ed4c9648373591
moyuh <mlroth94@gmail..com>
2022-11-12 21:59:21 -0700

    adds navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { SlHome } from "react-icons/sl"
+
+export default function Navigation() {
+    return (
+        <nav className="nav-info">
+            <ul className="nav-items">
+                <li>
+                    <SlHome/>
+                    <a
+                    href="#home">Home</a>
+                </li>
+                <li>
+                    <a
+                    href="#about">About Me</a>
+                </li>
+                <li>
+                    <a
+                    href="#portfolio">Portfolio</a>
+                </li>
+                <li>
+                    <a
+                    href="#resume">Resume</a>
+                </li>
+                <li>
+                    <a
+                    href="#contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
