e798667268c5261d3714941f19cf6743c28ad4e8
moyuh <mlroth94@gmail..com>
2022-11-13 00:51:14 -0700

    adds home componenet

---
+++ src/components/pages/Home.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+import { Container, Row, Col  } from "react-bootstrap";
+import Particle from "../Particle";
+
+function Type() {
+    return (
+        <Typewriter
+        options={{
+            strings:[
+                "Full-Stack Developer",
+                "Illustrator",
+                "Hard-worker",
+                "Gamer"
+            ],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 50,
+        }}
+        />
+    );
+}
+
+function Home() {
+    return(
+        <section>
+            <Container fluid className="home-section">
+                <Particle />
+                <Container className="content">
+                    <Row>
+                        <Col md="7" className="home-head">
+                            <h1> Thanks for dropping by!</h1>
+                            <h1>
+                                I am Monica
+                                Yuh
+                            </h1>
+                            <h2>
+                                I am a
+                            </h2>
+                            <div style={{ padding: 45, textAlign: "left" }}>
+                                <Type />
+                            </div>
+                        </Col>
+                        <Col md ="5">
+                            <img src= ".../assets/monica.Jpg" width="500" height="600" style={{ borderRadius: 100 }}/>
+                        </Col>
+                    </Row>
+
+                </Container>
+
+            </Container>
+        </section>
+    )
+}
+
+export default Home;
