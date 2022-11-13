5ae06cd3a7610d9d210b29eeed65afeaaee7ff97
moyuh <mlroth94@gmail..com>
2022-11-13 13:04:50 -0700

    adds resume page

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,28 @@
+import { Container, Row } from "react-bootstrap";
+import Particle from "../Particle";
+
+function Resume() {
+    return(
+        <div>
+            <Container fluid className= "resume-section">
+                <Particle />
+                <Row>
+                    <h2>
+                        {}
+                        <a
+                            href='../../assets/References_Resume.pdf'
+                            download='Peoples-Resume.pdf'
+                            className='slide res-size'
+        >
+          Downloadable Resume
+        </a>
+      </h2>
+                </Row>
+                <Row>
+                    <img src="../../assets/References_Resume.pdf"></img>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+export default Resume;
